79a2a6302a7cfa1e8d8578326c8f55bc89652248
Ladan Nazari <ladannazary@gmail.com>
2022-08-22 00:31:06 +0430

    Auth Me All Done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,16 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const userRouter = require('./users.js');
+
+router.use('/session', sessionRouter)
+
+router.use('/users', userRouter);
 
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
-  
 
 
 module.exports = router;
