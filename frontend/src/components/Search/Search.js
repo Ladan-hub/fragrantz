a0242d94cd1e77d01d07e984f224de2b5fabd6f8
Ladan Nazari <ladannazary@gmail.com>
2023-02-02 19:08:48 +0330

    search component initialized

---
+++ frontend/src/components/Search/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBar = () => {
+
+    return (
+        <div>
+            <input 
+            type="text"
+            name="search-input"
+            placeholder="Search perfumes by name"
+            
+            />
+        </div>
+    )
+}
