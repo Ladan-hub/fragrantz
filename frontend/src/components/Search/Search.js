bf93b77cbe7ad72368daa8ca4d3777e2c2863a58
Ladan Nazari <ladannazary@gmail.com>
2023-02-04 14:04:54 +0330

    turned search into a form

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Search.css";
 
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("");
 
-    const [searchInput, setSearchInput] = useState("");
+  const search = (e) => {
+    e.preventDefault();
+    
+  };
 
-    return (
-        <div className='search-container'>
-            
-            <input 
-            className='search-input'
-            type="text"
-            name="search-input"
-            placeholder="Search perfumes by name"
-            value={searchInput}
-            onChange={e => setSearchInput(e.target.value)}
-            />
-           
-            
-        </div>
-    )
-}
+  return (
+    <div className="search-container">
+      <form className="search-form" onSubmit={search}>
+        <input
+          className="search-input"
+          type="text"
+          name="search-input"
+          placeholder="Search perfumes by name"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          required
+        />
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
