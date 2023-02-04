5a65d6a49e92664aed3af65df43380643d01af1c
Ladan Nazari <ladannazary@gmail.com>
2023-02-04 16:55:07 +0330

    final fixes

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { fetchDetail } from "../../store/perfumes";
 import "./Search.css";
 
 const SearchBar = () => {
